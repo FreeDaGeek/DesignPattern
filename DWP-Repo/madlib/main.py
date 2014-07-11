01580d408e5cae4b2c9c82feff7848748a58770f
FreeDaGeek <free.rosas@gmail.com>
2014-07-11 04:38:10 -0400

    Errors
    Not showing up in the browser

--- DWP-Repo/madlib/main.py
+++ DWP-Repo/madlib/main.py
@@ -3,25 +3,13 @@ Free Rosas
 07/10/2014
 MadLib (Hey There Delilah-Remix)
 '''
+
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         print "Sorry Im not Sorry"
-
-music = dict()
-music = {"Band": "Plain White T's",
-         "Song": "Hey There Delilah",
-         "Album": "All That We Needed"}
-
-print "Help me learn the lyrics to " + music["Band"] + "Lyrics to " + music["Song"]
-
-
-
-
-
-
-
+    
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
