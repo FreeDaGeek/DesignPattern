d4c5a5d77e628c307ae0f73a87bfa11d262bcf74
FreeDaGeek <free.rosas@gmail.com>
2014-07-11 05:13:07 -0400

    ...

--- DWP-Repo/madlib/main.py
+++ DWP-Repo/madlib/main.py
@@ -8,8 +8,14 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.write('Hello world!M')
         print "Sorry Im not Sorry"
-    
+        music = dict()
+        music = {"Band": "Plain White T's",
+         "Song": "Hey There Delilah",
+         "Album": "All That We Needed"}
+
+        print "Help me learn the lyrics to " + music["Band"] + "Lyrics to " + music["Song"]
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
