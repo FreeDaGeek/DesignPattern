65f482f819086fa2c1e6ca7912d8187e03057ea6
FreeDaGeek <free.rosas@gmail.com>
2014-07-11 04:09:31 -0400

    First upload of MadLib

---
+++ DWP-Repo/madlib/main.py
@@ -0,0 +1,31 @@
+'''
+Free Rosas
+07/10/2014
+MadLib (Hey There Delilah-Remix)
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        print "Sorry Im not Sorry"
+
+music = dict()
+music = {"Band": "Plain White T's",
+         "Song": "Hey There Delilah",
+         "Album": "All That We Needed"}
+
+print "Help me learn the lyrics to " + music["Band"] + "Lyrics to " + music["Song"]
+
+
+
+
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
+
+
+
