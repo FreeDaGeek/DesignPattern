538fd8fc5f8c2b22bfc526b9b5cdeae2a10eee4c
FreeDaGeek <free.rosas@gmail.com>
2014-07-14 22:50:45 -0400

    Python--Classes

---
+++ DWP-Repo/classes-html/main.py
@@ -0,0 +1,34 @@
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        p = Page()
+        self.response.write(p.print_out())
+
+class Page(object):
+    def __init__(self):
+        self.title = "Welcome!"
+        self.css = "css/stylesheet.css"
+        self.head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>{self.title}</title>
+    <link href ="{self.css}" rel="stylesheet" text/css=text/css/>
+    </head>
+    <body>
+        """
+        self.body = "Welcome to my OOP Python Page!"
+        self.close = """
+    </body>
+</html>
+        """
+    def print_out(self):
+        all = self.head + self.body + self.close
+        all = all.format(**locals())
+        return all
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
