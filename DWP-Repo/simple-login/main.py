1eee1eee9b09d3ddee48a6c075ba1056963b854d
FreeDaGeek <free.rosas@gmail.com>
2014-07-15 22:32:04 -0400

    Uploading Simple Forms Final Video

--- DWP-Repo/simple-login/main.py
+++ DWP-Repo/simple-login/main.py
@@ -8,16 +8,25 @@ import webapp2  #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): #the fuction that starts everything
-        page = '''
-        <!DOCTYPE HTML>
+        page = '''<!DOCTYPE HTML>
 <html>
-    <head><title>Welcome!</title>
+    <head><title>Simple Form</title>
     </head>
-     <body> Hello to Everyone!
+     <body>
+         <form method="GET">
+           <label>Name: </label><input type="text" "name"="user" />
+            <label>Email: </label><input type="text" "name"="email" />
+            <input type="submit" value="Submit">
+         </form>
      </body>
-</html>
-        '''
-        self.response.write('Hello world!')
+</html> '''
+
+        if self.request.GET:
+            #Stores details we got from the forms
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+
+        self.response.write(page) #prints infromation.
 
 
 
