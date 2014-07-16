da56e968f17befd15a3e03eb32a59a01601895b0
FreeDaGeek <free.rosas@gmail.com>
2014-07-16 14:43:10 -0400

    form details
    Need to delete S.Login2

--- DWP-Repo/simple-login/main.py
+++ DWP-Repo/simple-login/main.py
@@ -14,8 +14,21 @@ class MainHandler(webapp2.RequestHandler): #declaring a class
      '''
         page_body ='''
             <form method="GET" action="">
-                <label>Name: </label><input type="text" "name"="user" />
-                <label>Email: </label><input type="text" "name"="email" />
+                <label>Name: </label><input type="text" name="user" /><br/>
+                <label>Email: </label><input type="text" name="email" /><br/>
+
+                <label>Send me emails..</label>
+                <select name="email_options" name="send">
+                    <option value="daily">Daily</option>
+                    <option value="weekly">Weekly</option>
+                    <option value="bi-weekly">Bi-Weekly</option>
+                    <option value="monthly">Monthly</option>
+                </select><br/>
+
+                 <label>Newsletter Topics</label><br/><input type="checkbox" names= "topics" value="New in tech">Tech New Update<br/>
+                  <input type="checkbox" names= "topics" value="Design Inspiration">Design Inspiration<br/>
+                 <input type="checkbox" names= "topics" value='Cool Codes "Cracked'>Cool Codes: "Cracked!"
+                   <label>
                 <input type="submit" value="Submit" />
             </form>
             '''
@@ -27,7 +40,8 @@ class MainHandler(webapp2.RequestHandler): #declaring a class
         if self.request.GET:
             user = self.request.GET['user']
             email = self.request.GET['email']
-            self.response.write(page_head + user + ' ' + email + page_body + page_close)
+            send = self.request.GET['send']
+            self.response.write(page_head + user + ' ' + email + ' ' + send + page_body + page_close)
         else:
             self.response.write(page_head + page_body + page_close) #prints infromation.
 
--- DWP-Repo/simple-login2/main.py
+++ DWP-Repo/simple-login2/main.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+import webapp2#use the webapp2 library
 
+class MainHandler(webapp2.RequestHandler): #delcaring a class
+    def get(self): #function that starts everything. Catalyst
+        page = '''<!DOCTYPE HTML>
+<html>
+    <head><title>Simple Login2</title></head>
+    <body>
+        <form method="GET">
+           <a href=""?email=mickey@disney.com&user="Micky">Mickey Mouse</a><br/>
+            <a href=""?email=Donald@disney.com>Donald Duck</a><br/>
+            <a href=""?email=Petee@disney.com&userPete</a><br/>
+           <a></a><br/>
+           <a></a><br/>
+           <a></a><br/>
+            </form></body>
+</html>'''
+        self.response.out.write(page)
+        #code goes here
+
+#never touch this
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
