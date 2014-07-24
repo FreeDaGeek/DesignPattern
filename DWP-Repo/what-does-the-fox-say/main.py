26b73751a5489eae76ecd9581b8b4ab6887c089f
FreeDaGeek <free.rosas@gmail.com>
2014-07-24 18:40:49 -0400

    Here is my last commit..till I find the missing

--- DWP-Repo/what-does-the-fox-say/main.py
+++ DWP-Repo/what-does-the-fox-say/main.py
@@ -7,17 +7,17 @@ What Does the Fox Say?
 import webapp2
 from animal import Panther, Dolphin, Fox
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         #method array
         array = ["Panther()", "Dolphin()", "Fox()"]
-        # self.response.write('Hello world!')
+        self.response.write('Hello world!')
 
         #html
-        a = Animals()
-        a.title = "What does the Fox Say?"
-        a.css = "css/style.css"
-        a.page_head = """
+        title = "What does the Fox Say?"
+        css = "css/style.css"
+        _page_head = """
 <!DOCTYPE HTML>
     <html>
         <head><title>What Does the Fox Say?</title>
@@ -25,18 +25,22 @@ class MainHandler(webapp2.RequestHandler):
         </head>
         <body>
             """
-        page_body = "" \
-                     "<header><h1>Animal:</h1><header>" \
-                             "<a href=?animals=" + array[0] + "><button class='first'>Panther</button>" \
-                             "<a href=?animals=" + array[1] + "><button class='second'>Dolphin</button>" \
-                             "<a href=?animals=" + array[2] + "><button class='third'>Fox</button>" \
-                     ""
+        _page_body = """<header><h1>Animal:</h1><header>
+                            <nav>
+                                <ul>
+                                    <a href=?animals=0" + array[0] + "><button class='first'>Panther</button>
+                                    <a href=?animals=1" + array[1] + "><button class='second'>Dolphin</button>
+                                    <a href=?animals=2" + array[2] + "><button class='third'>Fox</button>"
+                                </ul>
+                             </nav>
+                     """
 
-        page_close = """
+        _page_close = """
         </body>
 </html>"""
 
-        if self.request.GET: #uses the GET info to work on the page
+        if self.request.GET:
+         #uses the GET info to work on the page
             a = self.request.GET["animal"]
 
             if a == array[0]:
@@ -52,10 +56,11 @@ class MainHandler(webapp2.RequestHandler):
                 self.response.write(a.print_out())
 
         else:
-            total = page_head + page_body + page_close
+            total = _page_head + _page_body + _page_close
                 #prints details
             total = total.format(**locals())
-            self.response.write(total) #prints info onto the page
+            self.response.write(total)
+                #prints info onto the page
 
 
 app = webapp2.WSGIApplication([
