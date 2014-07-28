895843d418555c03eee722a3521d968c4f7f9031
FreeDaGeek <free.rosas@gmail.com>
2014-07-28 09:54:38 -0400

    moving over my 1st fox says into this file.

--- DWP-Repo/fox-says/main.py
+++ DWP-Repo/fox-says/main.py
@@ -28,52 +28,54 @@ class MainHandler(webapp2.RequestHandler):
         if self.request.GET: #button is selected show data
             animals = int(self.request.GET['animal'])
 
-name = animals[animal].name
-phylum = animals[animal].phylum
-classs = animals[animal].classs
-order = animals[animal].order
-family = animals[animal].family
-genus = animals[animal].genus
-img = animals[animal].img
-lifespan = animals[animal].lifespan
-habitat = animals[animal].habitat
-geolocation = animals[animal].geolocation
-sound = animals[animal].sound
+        name = animals[animal].name
+        phylum = animals[animal].phylum
+        classs = animals[animal].classs
+        order = animals[animal].order
+        family = animals[animal].family
+        genus = animals[animal].genus
+        img = animals[animal].img
+        lifespan = animals[animal].lifespan
+        habitat = animals[animal].habitat
+        geolocation = animals[animal].geolocation
+        sound = animals[animal].sound
 
-#info will have all the information populated into 3 sections
-info='''<div id="info">
-<h3>{name}</h3>
-<section id="properties">
-<p class="labels"><strong>Phylum:</strong></p>
-<p class="labels"><strong>Class:</strong></p>
-<p class="labels"><strong>Order:</strong></p>
-<p class="labels"><strong>Family:</strong></p>
-<p class="labels"><strong>Genus:</strong></p>
-<p class="labels"><strong>Lifespan:</strong></p>
-<p class="labels"><strong>Habitat:</strong></p>
-<p class="labels"><strong>Geolocation:</strong></p>
-<p class="labels"><strong>Sound:</strong></p>
-</section>
+        #info will have all the information populated into 3 sections
+        info='''<div id="info">
+        <h3>{name}</h3>
+        <section id="properties">
+            <p class="labels"><strong>Phylum:</strong></p>
+            <p class="labels"><strong>Class:</strong></p>
+            <p class="labels"><strong>Order:</strong></p>
+            <p class="labels"><strong>Family:</strong></p>
+            <p class="labels"><strong>Genus:</strong></p>
+            <p class="labels"><strong>Lifespan:</strong></p>
+            <p class="labels"><strong>Habitat:</strong></p>
+            <p class="labels"><strong>Geolocation:</strong></p>
+            <p class="labels"><strong>Sound:</strong></p>
+        </section>
 
-<section id="details">
-<p class="detail-info">{phylum}</p>
-<p class="detail-info">{classs}</p>
-<p class="detail-info">{order}</p>
-<p class="detail-info">{family}</p>
-<p class="detail-info">{genus}</p>
-<p class="detail-info">{lifespan}</p>
-<p class="detail-info">{habitat}</p>
-<p class="detail-info">{geolocation}</p>
-<p class="detail-info">{sound}</p>
-</section>
+        <section id="details">
+            <p class="detail-info">{phylum}</p>
+            <p class="detail-info">{classs}</p>
+            <p class="detail-info">{order}</p>
+            <p class="detail-info">{family}</p>
+            <p class="detail-info">{genus}</p>
+            <p class="detail-info">{lifespan}</p>
+            <p class="detail-info">{habitat}</p>
+            <p class="detail-info">{geolocation}</p>
+            <p class="detail-info">{sound}</p>
+        </section>
 
-<div id="img">
-<img src="{img}" title="Picture of {name}" alt="Picture of {name}" width="290" height="340" />
-</div> <!-- Closes "img" div -->
-</div>'''
-info = info.format(**locals())
+        <div id="img">
+            <img src="{img}" title="Picture of {name}" alt="Picture of {name}" width="290" height="340" />
+            </div> <!-- Closes "img" div -->
+    </div>'''
+    info = info.format(**locals())
 
-self.response.write(info)
+    self.response.write(info)
+
+
 self.response.write(page.footer)
 
 #abstract class-- used as a template
@@ -89,13 +91,104 @@ class Animals(object):
         self._lifespan = ''
         self._habitat = ''
         self._geolocation = ''
-@property
-def sound(self):
-return self.__sound
-@sound.setter
-def sound(self, new_sound):
-self.__sound = new_sound
 
+    @property
+    def sound(self):
+        return self.__sound
+
+    @sound.setter
+    def sound(self, new_sound):
+        self.__sound = new_sound
+
+#panther's info
+class Panther(Animals):
+    def __init__(self):
+        super(Panther, self).__init__()
+        #const. function -- super class
+
+        #data for the empty var in the super class above
+        self._title = "What does the Panther Say?"
+        self._phylum = 'Chordata'
+        self._classs = 'Mammalia'
+        self._order = 'Carnivora'
+        self._family = 'Felidae'
+        self._genus = 'Puma'
+        self._image = ''
+        self._lifespan = '12 years in the wild'
+        self._habitat = 'Wetland/Saw palmetto'
+        self._geolocation = 'Florida- Everglades'
+        self._list_open = "<h1>The Panther:</h1>"\
+                          "<ul><li>Phylum: " + self._phylum + "</li>"\
+                          "<li>Class: " + self._classs + "</li>"\
+                          "<li>Order: " + self._order + "</li>"\
+                          "<li>Family: " + self._family + "</li>"\
+                          "<li>Genus: " + self._genus + "</li>"\
+                          "<li>Image: " + self._image + "</li>"\
+                          "<li>Lifespan: " + self._lifespan + "</li>"\
+                          "<li>Habitat: " + self._habitat + "</li>"\
+                          "<li>Geolocation: " + self._geolocation + "</li>"\
+        
+        self._list_close = "</ul>"
+
+#dolphin's info
+class Dolphin(Animals):
+    def __init__(self):
+        super(Dolphin, self).__init__()
+        #const. function -- super class
+
+        #data for the empty var in the super class above
+        self._title = "What does the Dolphin Say?"
+        self._phylum = 'Chordata'
+        self._classs = 'Mammalia'
+        self._order = 'Cetacea'
+        self._family = 'Odontaceti'
+        self._genus = 'Delphinidae'
+        self._image = ''
+        self._lifespan = 'Up to 20 years'
+        self._habitat = 'Water'
+        self._geolocation = 'Throughout the oceans'
+        self._list_open = "<h1>The Dolphin:</h1>"\
+                          "<ul><li>Phylum: " + self._phylum + "</li>"\
+                          "<li>Class: " + self._classs + "</li>"\
+                          "<li>Order: " + self._order + "</li>"\
+                          "<li>Family: " + self._family + "</li>"\
+                          "<li>Genus: " + self._genus + "</li>"\
+                          "<li>Image: " + self._image + "</li>"\
+                          "<li>Lifespan: " + self._lifespan + "</li>"\
+                          "<li>Habitat: " + self._habitat + "</li>"\
+                          "<li>Geolocation: " + self._geolocation + "</li>"\
+
+        self._list_close = "</ul>"
+
+
+#fox's info
+class Fox(Animals):
+    def __init__(self):
+        super(Fox, self).__init__()
+        #const. function -- super class
+        #data for the empty var in the super class above
+        self._title = "What does the Fox Say?"
+        self._phylum = 'Chordata'
+        self._classs = 'Mammalia'
+        self._order = 'Carnivora'
+        self._family = 'Canidae'
+        self._genus = 'Vulpes'
+        self._image = ''
+        self._lifespan = '10 years in the wild'
+        self._habitat = 'Forest, deserts, grassland, mountains'
+        self._geolocation = 'All continent expect Antarctica'
+        self._list_open = "<h1>The Dolphin:</h1>"\
+                          "<ul><li>Phylum: " + self._phylum + "</li>"\
+                          "<li>Class: " + self._classs + "</li>"\
+                          "<li>Order: " + self._order + "</li>"\
+                          "<li>Family: " + self._family + "</li>"\
+                          "<li>Genus: " + self._genus + "</li>"\
+                          "<li>Image: " + self._image + "</li>"\
+                          "<li>Lifespan: " + self._lifespan + "</li>"\
+                          "<li>Habitat: " + self._habitat + "</li>"\
+                          "<li>Geolocation: " + self._geolocation + "</li>"\
+
+        self._list_close = "</ul>"
 
 
 app = webapp2.WSGIApplication([
