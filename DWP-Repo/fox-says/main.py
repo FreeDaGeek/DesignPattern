882638146782068e086eda0c233c8a933ee6a540
FreeDaGeek <free.rosas@gmail.com>
2014-07-28 14:47:57 -0400

    error due to extra spacing

--- DWP-Repo/fox-says/main.py
+++ DWP-Repo/fox-says/main.py
@@ -40,44 +40,43 @@ class MainHandler(webapp2.RequestHandler):
                         geolocation = animals[animal].geolocation
                         sound = animals[animal].sound
 
-                    #info will have all the information populated into 3 sections
-            info = '''
-                    <div id="info">
-                        <h3>{name}</h3>
-                        <section id="properties">
-                                <p class="labels"><strong>Phylum:</strong></p>
-                                <p class="labels"><strong>Class:</strong></p>
-                                <p class="labels"><strong>Order:</strong></p>
-                                <p class="labels"><strong>Family:</strong></p>
-                                <p class="labels"><strong>Genus:</strong></p>
-                                <p class="labels"><strong>Lifespan:</strong></p>
-                                <p class="labels"><strong>Habitat:</strong></p>
-                                <p class="labels"><strong>Geolocation:</strong></p>
-                                <p class="labels"><strong>Sound:</strong></p>
-                        </section>
+                        #info will have all the information populated into 3 sections
+                        info = '''
+                                <div id="info">
+                                    <h3>{name}</h3>
+                                    <section id="properties">
+                                            <p class="labels"><strong>Phylum:</strong></p>
+                                            <p class="labels"><strong>Class:</strong></p>
+                                            <p class="labels"><strong>Order:</strong></p>
+                                            <p class="labels"><strong>Family:</strong></p>
+                                            <p class="labels"><strong>Genus:</strong></p>
+                                            <p class="labels"><strong>Lifespan:</strong></p>
+                                            <p class="labels"><strong>Habitat:</strong></p>
+                                            <p class="labels"><strong>Geolocation:</strong></p>
+                                            <p class="labels"><strong>Sound:</strong></p>
+                                    </section>
 
-                        <section id="details">
-                                <p class="detail-info">{phylum}</p>
-                                <p class="detail-info">{classs}</p>
-                                <p class="detail-info">{order}</p>
-                                <p class="detail-info">{family}</p>
-                                <p class="detail-info">{genus}</p>
-                                <p class="detail-info">{lifespan}</p>
-                                <p class="detail-info">{habitat}</p>
-                                <p class="detail-info">{geolocation}</p>
-                                <p class="detail-info">{sound}</p>
-                        </section>
+                                    <section id="details">
+                                            <p class="detail-info">{phylum}</p>
+                                            <p class="detail-info">{classs}</p>
+                                            <p class="detail-info">{order}</p>
+                                            <p class="detail-info">{family}</p>
+                                            <p class="detail-info">{genus}</p>
+                                            <p class="detail-info">{lifespan}</p>
+                                            <p class="detail-info">{habitat}</p>
+                                            <p class="detail-info">{geolocation}</p>
+                                            <p class="detail-info">{sound}</p>
+                                    </section>
 
-                        <div id="img">
-                            <img src="{img}" title="Picture of {name}" alt=" {name}" width="290" height="340"/>
-                        </div> <!-- Closes "img" div -->
-                    </div>
-                    '''
-            info = info.format(**locals())
-            self.response.write(info)
+                                    <div id="img">
+                                        <img src="{img}" title="Picture of {name}" alt=" {name}" width="290" height="340"/>
+                                    </div> <!-- Closes "img" div -->
+                                </div>
+                                '''
+                        info = info.format(**locals())
+
+                        self.response.write(info)
             self.response.write(p.footer)
-
-
 
 class Animals(object):
     def __init__(self):
