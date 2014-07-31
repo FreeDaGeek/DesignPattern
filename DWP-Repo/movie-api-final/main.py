1df00ddc8f60fd717a80714501d622000d61af2f
FreeDaGeek <free.rosas@gmail.com>
2014-07-31 18:26:41 -0400

    Completed 1st ModelHandler
    Retyping everything from my last f.project attempt

--- DWP-Repo/movie-api-final/main.py
+++ DWP-Repo/movie-api-final/main.py
@@ -12,7 +12,21 @@ class MainHandler(webapp2.RequestHandler):
         p = FormPage()
         #makes the page load correctly w/ GET
         if self.request.GET:
-            pass
+            #create our model
+            mm = MovieModel()
+            #sends our user input from the url to the model
+            mm.title = self.request.GET['title']
+            #connect to the API
+            mm.call_api()
+            #creates view
+            mv = MovieView()
+            #shares data obj form model class to the view
+            mv.mdos = mm.dos
+            #shares the view content to the form body
+            p._body = mv.content
+        #calls the print_out to display on page
+        self.response.write(p.print_out())
+
 
 #the View
 class MovieView(object):
