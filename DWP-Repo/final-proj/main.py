492d1a21a96749dd3d8c247aa1ce9959fa2b57df
FreeDaGeek <free.rosas@gmail.com>
2014-07-31 00:06:37 -0400

    FormPage() & Trying to add the details view/model

--- DWP-Repo/final-proj/main.py
+++ DWP-Repo/final-proj/main.py
@@ -3,23 +3,58 @@ Free Rosas
 
 '''
 import webapp2
+import urllib2
+from xml.dom import minidom
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = FormPage()
+        p.inputs = [['beer', 'text', 'beer type'], ['submit', 'submit']]
 
+        if self.request.GET:
+            #creates our model
+            bm =BeerModel()
+            bm.beer = self.response.GET['beer']
+            bm.call_api()
+
+            #creates out view
+            bv = BeerView()
+            #takes data object from model class and give it to view
+            bv.bdos = bm.dos
+            p._body = bv.content
+
+        self.response.write(p.print_out())
 
       #this class shows the data
 class BeerView(object):
     def __init__(self):
-        pass
+        self.__bdos =[]
+        self.__content = '<br/>'
+
+    def update(self):
+        for do in self.__bdos:
+            pass
 
 
 
 
 class BeerModel(object):
     def __init__(self):
-        pass
+        self.__url ='https://www.thebeerspot.com/api/'
+        self._beer_type = ''
+        self._beer_name = ''
+
+
+
+    def __call_api(self):
+        request = urllib2.Request(self.__url + self._beer_type + self._beer_name)
+        opener = urllib2.build_opener()
+        results = opener.open(request)
+        #parse data
+        self.xmldoc = minidom.parse(results)
+
+
 
 
 
@@ -36,7 +71,7 @@ class Page(object):
 <!DOCTYPE HTML>
 <html>
     <head>
-        <title>Weather App</title>
+        <title>Find My Beer</title>
         <link href="css/main.css" rel="stylesheet" type="text/css" />
     </head>
     <body>'''
@@ -47,14 +82,39 @@ class Page(object):
 </html>'''
 
 
+def print_out(self):
+        return self._head + self._body + self._close
 
 
+class FormPage(Page):
+    def __init__(self):
+        super(FormPage, self).__init__()
+        self._form_open = '<form method="GET">'
+        self._form_close = '</form>'
+
+        self.__inputs = []
+        self._form_inputs = ''
+
+    @property
+    def inputs(self):
+        pass
+
+    @inputs.setter
+    def inputs(self, arr):
+
+        self.__inputs = arr
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+
+            try:
+                self._form_inputs += '" placeholder="' + item[2] + '" />'
+            except:
+                self._form_inputs += '" />'
+
+    def print_out(self):
+        return self._head + "Find My Beer!"+ self._form_open + self._form_inputs + self._form_close + self._body + self._close
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
-
-
-
-
